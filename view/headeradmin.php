7705274db7b69059a81f7bf020f0bfed920e030a
Bagas Uwaidha <bagasuwaidha007@gmail.com>
2023-05-09 11:28:03 +0700

    h

--- view/headeradmin.php
+++ view/headeradmin.php
@@ -7,13 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!----======== CSS ======== -->
-    <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!----===== Iconscout CSS ===== -->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
     <link rel="stylesheet" href="view/nganu.css" />
+    <link rel="stylesheet" href="style.css" />
 
     <title>Admin Dashboard Panel</title>
 </head>
